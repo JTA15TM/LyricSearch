c9dcfb49248ed8cde3cdbe29754ce5f796c101e7
JTA Team <64577007+JTA15TM@users.noreply.github.com>
2020-08-21 17:41:04 +0300

    Add files via upload

--- authScripts.js
+++ authScripts.js
@@ -3,16 +3,46 @@ LoadAuthConfig = function()
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "client_config.json", true);
 
-    xhr.send(null);
+    onStartLoading();
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState != 4) return;
 
         if (xhr.status != 200) {
-          alert(xhr.status + ': ' + xhr.statusText);
+            onErrorLoading();
         } else {
-          alert(xhr.responseText);
+            onSuccessLoading(xhr.responseText);
         }
-      
       }
+      xhr.send(null);
+}
+
+onload = function()
+{
+    LoadAuthConfig();
+}
+
+onStartLoading = function()
+{
+    var component = "<img width=\"10%\" height=\"10%\" src=\"gifs/progress_bar.gif\"/>";
+    document.getElementById("auth_state_view").innerHTML = component;
+}
+
+onErrorLoading = function()
+{
+    var component = "<p id=\"auth_error_title\">При загрузке данных произошла ошибка!</p>"
+    + "<div id=\"auth_retry_button\" onclick=\"LoadAuthConfig();\">Повторить попытку</div>";
+    document.getElementById("auth_state_view").innerHTML = component;
+}
+
+onSuccessLoading = function(response)
+{
+    var item = JSON.parse(response);
+    var scopes = "";
+    for(var i = 0; i < item.scopes.length; i++)
+    {
+        scopes += item.scopes.scope;
+    }
+    alert(scopes);
+    var link = "https://api.genius.com/oauth/authorize?client_id=" + item.client_id + "&redirect_uri=" + item.redirect_uri + "&scope=" + scopes + "&state=" + item.state + "&response_type=" + item.response_type;
 }
