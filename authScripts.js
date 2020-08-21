0b39c7d55be1f376b387ec0cbd68459bd5b45d8f
JTA Team <64577007+JTA15TM@users.noreply.github.com>
2020-08-21 14:41:43 +0300

    Add files via upload

--- authScripts.js
+++ authScripts.js
@@ -4,4 +4,15 @@ LoadAuthConfig = function()
     xhr.open("GET", "client_config.json", true);
 
     xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) return;
+
+        if (xhr.status != 200) {
+          alert(xhr.status + ': ' + xhr.statusText);
+        } else {
+          alert(xhr.responseText);
+        }
+      
+      }
 }
